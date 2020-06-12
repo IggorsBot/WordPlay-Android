228ec1ef11a052c7f10f293fa75bd3023aa2a579
Igor <IggorsBot@gmail.com>
2020-06-13 00:01:54 +0300

    refactoring

--- app/src/main/java/com/gogabot/englishwords/MainActivity.kt
+++ app/src/main/java/com/gogabot/englishwords/MainActivity.kt
@@ -2,9 +2,11 @@ package com.gogabot.englishwords
 
 import android.R.attr.x
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.res.Resources
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.util.TypedValue
 import android.view.Gravity
 import android.view.MotionEvent
@@ -13,9 +15,11 @@ import android.widget.*
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
+import com.gogabot.englishwords.dictionary.Dictionary
+import com.gogabot.englishwords.dictionary.IDictionary
+
 
 val TAG = "MainActivity"
-
 
 private val Int.dp: Int
     get() = (this * Resources.getSystem().displayMetrics.density + 0.5f).toInt()
@@ -25,12 +29,11 @@ val Float.dp: Int
 
 
 class MainActivity : AppCompatActivity() {
-
     lateinit var dictListScrollView : ScrollView
     lateinit var dictListGridView: GridLayout
     lateinit var menuTip : RelativeLayout
 
-    var dictListArray: Array<CardView> = emptyArray()
+    var dictListArray: MutableList<IDictionary> = mutableListOf<IDictionary>()
 
     private val deltaAlpha: Float = 0.02f
 
@@ -69,121 +72,35 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
-        createDictionary()
-        createDictionary()
-
+        createDictionaries(this)
+        showDictionaries()
     }
 
-    private fun createDictionaryText(): TextView {
-        // Initialize a new TextView to put in LinearLayout
+    fun createDictionaries(context: Context) {
+        for (i in 1..10) {
+            val dictionary: IDictionary = Dictionary(context)
+            dictionary.id = i
+            dictionary.name = "Cловарь $i."
 
-        val dictionaryText: TextView = TextView(this)
-
-        val params: LinearLayout.LayoutParams =
-            LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT
-            )
-
-        params.setMargins(
-            0,
-            resources.getDimension(R.dimen.card_text_margin).dp,
-            0,
-            0
-        )
-        dictionaryText.layoutParams = params
-
-        dictionaryText.setTextSize(
-            TypedValue.COMPLEX_UNIT_PX,
-            resources.getDimension(R.dimen.dictionary_text_size)
-        )
-
-        dictionaryText.setTextColor(Color.parseColor("#6f6f6f"));
-        dictionaryText.text = "Calendar"
-        return dictionaryText
+            dictListArray.add(dictionary)
+        }
     }
-    private fun createDictionaryImage(): ImageView {
-        // Initialize a new ImageView to put in LinearLayout
-
-        val dictionaryImage: ImageView = ImageView(this)
-
-        val params: LinearLayout.LayoutParams =
-            LinearLayout.LayoutParams(
-                resources.getDimension(R.dimen.card_image_size).dp,
-                resources.getDimension(R.dimen.card_image_size).dp
-            )
-
-        dictionaryImage.layoutParams = params
-        dictionaryImage.setImageResource(R.mipmap.ic_launcher_round)
-
-        return dictionaryImage
-    }
-
-    private fun createLinearLayoutForDictionary(): LinearLayout {
-        // Initialize a new LinearLayout to put in CardView
-
-        val dictionaryLinearLayout: LinearLayout = LinearLayout(this)
-        val params: LinearLayout.LayoutParams =
-            LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.MATCH_PARENT
-            )
-        dictionaryLinearLayout.layoutParams = params
-
-        dictionaryLinearLayout.setPadding(
-            0,
-            resources.getDimension(R.dimen.dictionary_padding).dp,
-            0,
-            0
-        )
-
-        dictionaryLinearLayout.orientation = LinearLayout.VERTICAL
-        dictionaryLinearLayout.gravity = Gravity.CENTER
-
-        dictionaryLinearLayout.addView(createDictionaryImage())
-        dictionaryLinearLayout.addView(createDictionaryText())
-
-        return dictionaryLinearLayout
-    }
-
-    private fun createDictionary() {
-        // For converting dp to pixels
-
-        val dictionary: CardView = CardView(this)
-
-        val params: GridLayout.LayoutParams = GridLayout.LayoutParams(
-            GridLayout.spec(
-                GridLayout.UNDEFINED, GridLayout.FILL, 1f
-            ),
-            GridLayout.spec(GridLayout.UNDEFINED, GridLayout.FILL, 1f)
-        )
-        params.height = LinearLayout.LayoutParams.WRAP_CONTENT;
-        params.width  = LinearLayout.LayoutParams.WRAP_CONTENT;
-
-        params.setMargins(
-            resources.getDimension(R.dimen.card_margin_full).dp,
-            resources.getDimension(R.dimen.card_margin_full).dp,
-            resources.getDimension(R.dimen.card_margin_full).dp,
-            resources.getDimension(R.dimen.card_margin_full).dp
-        )
-
-        dictionary.layoutParams = params
-        dictionary.cardElevation = resources.getDimension(R.dimen.card_elevation)
-        dictionary.radius = resources.getDimension(R.dimen.card_corner_radius)
-
-        dictionary.addView(createLinearLayoutForDictionary())
-
-        dictListGridView.addView(dictionary)
+    fun showDictionaries() {
+        for (i in 0..9) {
+            dictListGridView.addView(dictListArray[i].dictionaryView)
+        }
     }
 
     fun smoothScrolling(yAtTheTimeReleasing: Float = 0.0f) {
-        if(yAtTheTimePress - yAtTheTimeReleasing < menuTip.height && this.yScrollAtTheTimePress < 100) {
+        if(yAtTheTimePress - yAtTheTimeReleasing < menuTip.height
+            && this.yScrollAtTheTimePress < 100
+            && yAtTheTimePress != yAtTheTimeReleasing) {
             dictListScrollView.post(Runnable { dictListScrollView.smoothScrollTo(x, menuTip.height) })
         }
 
         if (yAtTheTimeReleasing > yAtTheTimePress && dictListScrollView.scrollY < menuTip.height) {
             dictListScrollView.post(Runnable { dictListScrollView.smoothScrollTo(x, 0) })
-            menuTip.alpha = 1f
+            menuTip.alpha = maxAlphaValue
         }
     }
 
